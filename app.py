f967dd0eee611f2cd7f0ad1fe2dec6ec62d36499
princyiakov <princy@giskard.ai>
2023-04-11 12:10:52 +0800

    model.pkl updated with the pipeline code

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-from flask import Flask, render_template, request
-import joblib
-import pandas as pd
-import numpy as np
+from flask import Flask, request, render_template
+
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 app = Flask(__name__)
-
-model = joblib.load(open('model/rf_model.pkl', 'rb'))
-cat_fillnan = joblib.load(open('model/cat_fillnan.pkl', 'rb'))
-num_fillnan = joblib.load(open('model/num_fillnan.pkl', 'rb'))
-encoder = joblib.load(open('model/endoder.pkl', 'rb'))
-scalar = joblib.load(open('model/scalar.pkl', 'rb'))
 
 
 @app.route('/')
@@ -17,43 +10,28 @@ def home():
     return render_template('home.html')
 
 
-def preprocess(dataframe):
-    dataframe = dataframe[['education', 'investment_account', 'income', 'family_size', 'creditc_avg_spent', 'mortgage']]
-    cols_to_encode = ['education', 'investment_account']
-    cols_to_scale = ['income', 'family_size', 'creditc_avg_spent', 'mortgage']
-
-    dataframe = dataframe.replace('', np.nan)
-
-    # Define the data types of the columns to be changed
-    to_change = {'family_size': 'int64', 'investment_account': 'int64',
-                 'income': 'float64',
-                 'creditc_avg_spent': 'float64',
-                 'mortgage': 'float64'
-                 }
-    # Create a new dictionary with the changed data types
-    for col, dtype in to_change.items():
-        dataframe[col] = np.array(dataframe[col], dtype=dtype)
-
-    dataframe[cols_to_encode] = cat_fillnan.transform(dataframe[cols_to_encode])
-    dataframe[cols_to_scale] = num_fillnan.transform(dataframe[cols_to_scale])
-    dataframe[cols_to_encode] = encoder.transform(dataframe[cols_to_encode])
-    dataframe[cols_to_scale] = scalar.transform(dataframe[cols_to_scale])
-
-    return dataframe
-
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.form.to_dict()
+    data = CustomData(
+        education=request.form.get('education'),
+        investment_account=int(request.form.get('investment_account')),
+        income=request.form.get('income'),
+        family_size=request.form.get('family_size'),
+        creditc_avg_spent=float(request.form.get('creditc_avg_spent')),
+        mortgage=float(request.form.get('mortgage'))
 
-    # Convert the input data to a pandas dataframe
-    df = pd.DataFrame(data, index=[0])
-    df = preprocess(df)
+    )
 
-    output = model.predict(df)
-    prediction = "Will take Personal Loan" if output[0] == 1 else "Will NOT take Personal Loan"
-    return render_template("home.html", prediction_text=f'FINAL PREDICTION: {prediction}')
+    pred_df = data.get_data_as_data_frame()
+    print(pred_df)
+    print("Before Prediction")
+
+    predict_pipeline = PredictPipeline()
+    print("Mid Prediction")
+    results = predict_pipeline.predict(pred_df)
+    print("after Prediction")
+    return render_template('home.html', prediction_text=f'FINAL PREDICTION: {results}')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=3000, debug=True)
